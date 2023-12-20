1e8bd9ee48513f818e133236c97fa818ae40b255
cynelax20 <cynelax@DESKTOP-EFOLC7K>
2023-12-20 17:06:48 -0600

    inital message

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Docker
+            Here is a major veersion change
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
